744f421e8153c973df7ea1a2814bdb4eb5cea368
FATEH02 <102332591+FATEH02@users.noreply.github.com>
2024-07-26 11:59:41 +0530

    Create RetrofitClient.kt

---
+++ RetrofitClient.kt
@@ -0,0 +1,14 @@
+package com.example.myshoppinglistapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://maps.googleapis.com/"
+
+    fun create():GeocodingApiService
+    {
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        return retrofit.create(GeocodingApiService::class.java)
+    }
+}
